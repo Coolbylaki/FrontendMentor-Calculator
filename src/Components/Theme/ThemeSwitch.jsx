a29bdc44a1b03291ec53d6a6c1976c7f10864970
Lazar Stojanović <lakisd98@gmail.com>
2023-04-06 22:56:10 +0200

    Update: theme roller label position

--- src/Components/Theme/ThemeSwitch.jsx
+++ src/Components/Theme/ThemeSwitch.jsx
@@ -6,7 +6,7 @@ const ThemeSwitch = (props) => {
 			<h1 className="text-white text-calc">calc</h1>
 			<div className="flex items-center">
 				<div className="bg-T1-toggleBg h-[1.5rem] w-[75px] rounded-[50rem] relative flex justify-center">
-					<div className="w-[30%] relative">
+					<div className="w-[30%] relative left-2">
 						<label htmlFor="theme1" className="w-full absolute bottom-[100%] left-0 text-xs cursor-pointer text-white">
 							1
 						</label>
@@ -31,7 +31,7 @@ const ThemeSwitch = (props) => {
 							className="mx-auto h-full w-[30px] opacity-0 block cursor-pointer"
 						/>
 					</div>
-					<div className="w-[30%] relative text-right">
+					<div className="w-[30%] relative text-right right-2">
 						<label htmlFor="theme3" className="w-full absolute bottom-[100%] left-0 text-xs cursor-pointer text-white">
 							3
 						</label>
@@ -44,7 +44,7 @@ const ThemeSwitch = (props) => {
 						/>
 					</div>
 					<div
-						className={`pointer-events-none w-[1rem] h-[1rem] bg-T1-togKey rounded-[50rem] absolute top-[5px] ${classes["left-calc"]}`}></div>
+						className={`pointer-events-none w-[1rem] h-[1rem] bg-T1-togKey rounded-[50rem] absolute top-[4px] ${classes["left-calc"]}`}></div>
 					<h3 className="absolute text-xs left-[-3.5rem] text-white top-[8px]">THEME</h3>
 				</div>
 			</div>
