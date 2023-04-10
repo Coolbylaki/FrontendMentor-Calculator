ab6b2deb7f192611639577601869021a8e84baa1
Lazar Stojanović <lakisd98@gmail.com>
2023-04-10 07:01:07 +0200

    Fix: calculator screen overflow

--- src/Components/CalculatorBody/Screen.jsx
+++ src/Components/CalculatorBody/Screen.jsx
@@ -4,13 +4,16 @@ import CalcContext from "../../store/calc-context";
 const Screen = (props) => {
 	const ctx = useContext(CalcContext);
 
+	const leftSide = parseInt(ctx.total.split(".")).toLocaleString();
+	const rightSide = ctx.total.split(".")[1] ? ctx.total.split(".")[1] : "";
+
+	const formatNumber = rightSide ? leftSide + "." + rightSide : leftSide;
+
 	console.log(ctx);
 
 	return (
-		<section className="bg-screenBg rounded-lg mt-4">
-			<h2 className="text-right text-mainText text-calc py-4 pr-5">
-				{ctx.total ? parseFloat(ctx.total).toLocaleString() : 0}
-			</h2>
+		<section className="bg-screenBg rounded-lg mt-4 px-1">
+			<h2 className="text-right text-mainText text-calc py-4 pr-5 overflow-auto">{ctx.total ? formatNumber : 0}</h2>
 		</section>
 	);
 };
